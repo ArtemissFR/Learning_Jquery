a132532760b3a98857c95fa9e73182fe7cdf5df0
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-07 11:09:43 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -18,15 +18,41 @@ $(document).ready(function(){
     
     });
 
+// ------------------------------------------------------------------------------- BUTTON (En savoir plus) - Configuration -------------------------------------------------------------------------------
+
     var liens = [
         'https://www.toutelasignaletique.com/21347/chiffre-1-en-aluminium-2-dimensions-5-coloris.jpg',
         'https://media.istockphoto.com/id/968526968/photo/number-2-3d-clean-red-isolated-on-white.jpg?s=612x612&w=0&k=20&c=MVQ8sxh-xTje4v1N_6n2gZnEABVsCm_tIarVNtjPBpQ=',
         'https://www.toutelasignaletique.com/21357-thickbox_default/chiffre-3-en-alu-couleur-et-dimensions-au-choix.jpg'
     ];
 
-    // Ajouter un bouton "En savoir plus" à la fin de chaque article avec un lien différent
+    // Ajouter un bouton "En savoir plus" à la fin de l'article 1
     $('article').each(function (index) {
-        var articleFooter = $(this).find('.footer1','.footer2','.footer3');
+        var articleFooter = $(this).find('.footer1');
+        var enSavoirPlusButton = $('<button class="en-savoir-plus" type="button">En savoir plus</button>');
+
+        enSavoirPlusButton.click(function () {
+            window.location.href = liens[index];
+        });
+
+        articleFooter.append(enSavoirPlusButton);
+    });
+
+    // Ajouter un bouton "En savoir plus" à la fin de l'article 2
+    $('article').each(function (index) {
+        var articleFooter = $(this).find('.footer2');
+        var enSavoirPlusButton = $('<button class="en-savoir-plus" type="button">En savoir plus</button>');
+
+        enSavoirPlusButton.click(function () {
+            window.location.href = liens[index];
+        });
+
+        articleFooter.append(enSavoirPlusButton);
+    });
+
+    // Ajouter un bouton "En savoir plus" à la fin de l'article 3
+    $('article').each(function (index) {
+        var articleFooter = $(this).find('.footer3');
         var enSavoirPlusButton = $('<button class="en-savoir-plus" type="button">En savoir plus</button>');
 
         enSavoirPlusButton.click(function () {
