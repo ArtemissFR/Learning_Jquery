344f3a3349b4a7e6627251bc4a853d558e920121
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-07 10:52:03 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
     var articlesLi = $('<li><a href="#articles">Articles</a></li>');
     var formulaireLi = $('<li><a href="#formulaire">Formulaire</a></li>');
     $('ul').append(articlesLi, formulaireLi);
- //   $('ul').append('<li>li 1</li><br><li>li 2</li>');
 
     var a = $("article").find("a");
     a.attr({
@@ -17,6 +16,23 @@ $(document).ready(function(){
     
     });
 
+    var liens = [
+        'https://www.toutelasignaletique.com/21347/chiffre-1-en-aluminium-2-dimensions-5-coloris.jpg',
+        'https://media.istockphoto.com/id/968526968/photo/number-2-3d-clean-red-isolated-on-white.jpg?s=612x612&w=0&k=20&c=MVQ8sxh-xTje4v1N_6n2gZnEABVsCm_tIarVNtjPBpQ=',
+        'https://www.toutelasignaletique.com/21357-thickbox_default/chiffre-3-en-alu-couleur-et-dimensions-au-choix.jpg'
+    ];
+
+    // Ajouter un bouton "En savoir plus" à la fin de chaque article avec un lien différent
+    $('article').each(function (index) {
+        var articleFooter = $(this).find('.footer');
+        var enSavoirPlusButton = $('<button class="en-savoir-plus" type="button">En savoir plus</button>');
+
+        enSavoirPlusButton.click(function () {
+            window.location.href = liens[index];
+        });
+
+        articleFooter.append(enSavoirPlusButton);
+    });
 
     $('.button1').click(function () {
         var visibilite = $('.footer').css('display');
